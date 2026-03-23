85651007b991eef5cd0bdba2db6505800608aa61
AchinthaThennakoon <achinthathennakoon97@gmail.com>
2026-03-23 21:45:37 +0530

    install tailwind css

--- src/pages/Loginpage.tsx
+++ src/pages/Loginpage.tsx
@@ -1,7 +1,7 @@
 export function Loginpage() {
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1 className="bg-blue-500 text-white p-4">Login Page</h1>
       {/* Add your login form here */}
     </div>
   );
