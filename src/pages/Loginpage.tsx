4c0f047589a1a25abea9e8e9654633f47414eb08
AchinthaThennakoon <achinthathennakoon97@gmail.com>
2026-04-02 21:11:20 +0530

    fix styling

--- src/pages/Loginpage.tsx
+++ src/pages/Loginpage.tsx
@@ -3,18 +3,18 @@ export function Loginpage() {
     <div className="flex justify-center items-center h-screen">
       <div className="bg-gray-100 p-8 rounded-md">
         <h1 className="block text-2xl font-bold">Login</h1>
-        <form className="flex flex-col gap-4 mt-0.5" action="">
+        <form className="flex flex-col gap-4 mt-4" action="">
           <input
             type="text"
             placeholder="Username"
-            className="bg-white border border-gray-300 py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="bg-white border border-gray-300 py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="password"
             placeholder="Password"
-            className="bg-white border border-gray-300 py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="bg-white border border-gray-300 py-2 px-4 focus:outline-none rounded-md focus:ring-2 focus:ring-blue-500"
           />
-          <div className="flex flex-row gap-4">
+          <div className="flex flex-row justify-between gap-4">
             <button
             type="submit"
             className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
